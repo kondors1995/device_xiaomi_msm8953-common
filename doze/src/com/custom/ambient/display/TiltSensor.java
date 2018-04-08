c655483df5ad3828ea3d6aa56cecf2c1bfa9467d
Bruno Martins <bgcngm@gmail.com>
2018-04-08 15:18:44 +0100

    msm8953-common: Doze: Minor code simplification and clean up
    Change-Id: Idff05f2626138c96dccd3ab4c26c4df1be40a606

--- doze/src/com/custom/ambient/display/TiltSensor.java
+++ doze/src/com/custom/ambient/display/TiltSensor.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2015 The CyanogenMod Project
+ *               2017-2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,21 +36,16 @@ public class TiltSensor implements SensorEventListener {
     private static final int BATCH_LATENCY_IN_MS = 100;
     private static final int MIN_PULSE_INTERVAL_MS = 2500;
 
-    private PowerManager mPowerManager;
     private SensorManager mSensorManager;
     private Sensor mSensor;
-    private WakeLock mSensorWakeLock;
     private Context mContext;
 
     private long mEntryTimestamp;
 
     public TiltSensor(Context context) {
         mContext = context;
-        mPowerManager = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
-        mSensorManager = (SensorManager) mContext.getSystemService(Context.SENSOR_SERVICE);
+        mSensorManager = mContext.getSystemService(SensorManager.class);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_TILT_DETECTOR);
-        mSensorWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
-                "SensorWakeLock");
     }
 
     @Override
