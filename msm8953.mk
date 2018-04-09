709aa71acc71ccd4e696780a2f5d45a9506017e1
xyyx <xyyx@mail.ru>
2018-04-09 12:38:40 +0300

    mido: Add XiaomiParts
    Signed-off-by: TheHungarianHorntail <sayan7848@gmail.com>
    Signed-off-by: sayan7848 <sayan7848@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -358,3 +358,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+# Extras
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
