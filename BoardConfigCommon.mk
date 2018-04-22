3de162f5949b929ecfb67e021c1f576168136976
sayan7848 <sayan7848@gmail.com>
2018-04-22 13:14:16 +0000

    Use Linaro 7.3x for kernel
    Signed-off-by: sayan7848 <sayan7848@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -45,6 +45,10 @@ BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
 
+# Linaro
+KERNEL_TOOLCHAIN := /home/sayan7848/aarch64-linux-android/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-opt-linux-android-
+
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
 
